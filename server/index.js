f3080ee832bed771617d6fff76322ce9021b6b9f
Tian Yu <gladaitormaximusty99@gmail.com>
2021-02-19 00:02:43 +0800

    新增分類接口及前後端完成，跨域問題修復完成

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
+//後端允許跨域(跨域要寫在上方喔！)：
+app.use(require('cors')())
+//允許後端接受 JSON 格式
+app.use(express.json())
 //連接 Router 模塊
 require('./routes/admin/index')(app)
 //連接 Mongodb
 require('./plugin/db')(app)
-//後端允許跨域：
-app.use(require('cors')())
-//允許後端接受 JSON 格式
-app.use(express.json())
+
 
 app.listen(3000,()=>{
     console.log('App is listen on 3000')
